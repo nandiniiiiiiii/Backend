b8461cea8aa80e15d7eb6833516e9b79fccb8c8a
nandiniiiiiiii <116261814+nandiniiiiiiii@users.noreply.github.com>
2024-01-24 23:49:45 +0530

    commit five

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,8 +1,26 @@
-import {v2 as cloudinary} from 'cloudinary';
+import { v2 as cloudinary } from 'cloudinary';
 import fs from 'fs';         //to perform relate to files
 
-cloudinary.config({ 
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME , 
-    api_key: process.env.CLOUDINARY_API_KEY , 
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
-  });
+});
+
+//transfering file from local to server
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        //uploading file via cloudinary
+        if(!localFilePath) return null;
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        console.log("File uploded successfuly ",response.url)
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath)            //remove locally saved temporary file as upload operation got failed 
+        return null;
+    }
+}
+
+export {uploadOnCloudinary}
